3af955993cc872646d9d744dd5762877f4ba332b
gabri <gabrielrjed8@gmail.com>
2026-04-13 11:53:10 -0300

    feat: criando entity usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './Usuário/entities/usuario.entity';
+import { UsuarioModule } from './Usuário/usuario.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_usuarios',
+      entities: [Usuario],
+      synchronize: true, 
+      logging: true,
+    }),
+    UsuarioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
